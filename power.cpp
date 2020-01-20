fab09b61eb66fcc96b33cd5296629e899e673762
weiss21 <waisalirobleh@gmail.com>
2020-01-20 09:26:17 +0000

    power done

--- power.cpp
+++ power.cpp
@@ -1,6 +1,6 @@
 /*
  * Title: power.cpp
- * Abstract: compute 2n for a nonnegative integer n. Use resursive technique.
+ * Abstract: compute 2n for a nonnegative integer n. Use recursive technique.
  * Author: Wais Robleh
  * ID: 1624
  * Date: 01/19/2020
@@ -11,14 +11,28 @@
 #include <cstring>
 
 using namespace std;
+int recursive(int i);// so it can run. Must call before main.
 
 int main() {
     
     int number;
+    int result;
     
     cout << "Enter a number: ";
     cin >> number;
     
+    result = recursive(number);
+    //output below
+    cout << "Result: " << result << endl;
+    
     
     return 0;
+}
+
+int recursive(int n){
+    if(n == 0){
+        return 1;
+    } else {
+        return 2 * recursive(n - 1); //recursive formula
+    }
 }
