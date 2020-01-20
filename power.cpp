a88cb2980c5aadc367a76249bccfe2234e0b7988
weiss21 <waisalirobleh@gmail.com>
2020-01-20 09:01:41 +0000

    first file

---
+++ power.cpp
@@ -0,0 +1,24 @@
+/*
+ * Title: power.cpp
+ * Abstract: compute 2n for a nonnegative integer n. Use resursive technique.
+ * Author: Wais Robleh
+ * ID: 1624
+ * Date: 01/19/2020
+ */
+ 
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    
+    int number;
+    
+    cout << "Enter a number: ";
+    cin >> number;
+    
+    
+    return 0;
+}
